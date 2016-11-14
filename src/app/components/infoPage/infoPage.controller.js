e0de635d550678bb8f2552da9ae90201098205ae
Ilya Simenko <ilya42@gmail.com>
2016-11-14 11:58:02 +0200

    implement help and about pages

--- src/app/components/infoPage/infoPage.controller.js
+++ src/app/components/infoPage/infoPage.controller.js
@@ -1,6 +1,13 @@
+import help from './help.html';
+import about from './about.html';
+
 export default class {
   constructor($stateParams) {
     'ngInject';
-    this.page = $stateParams.page || 'help';
+
+    this.pages = {};
+    this.pages.help = help;
+    this.pages.about = about;
+    this.content = this.pages[$stateParams.page] || this.pages.help;
   }
 };
