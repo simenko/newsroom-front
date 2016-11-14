d51172933c659812c7b27d856bd3c44a2eaf5dbf
Ilya Simenko <ilya42@gmail.com>
2016-11-14 10:40:24 +0200

    upgrade story controller

--- src/app/components/story/story.controller.js
+++ src/app/components/story/story.controller.js
@@ -1,27 +1,34 @@
 export default class {
-  constructor($stateParams, stories, session) {
+  constructor($stateParams, stories, session, users) {
     'ngInject';
 
     this.$stateParams = $stateParams;
+    this.users = users;
     this.stories = stories;
     this.session = session;
-    this.editMode = true;
+    this.editMode = false;
     this.storyBuffer = {};
+    this.storyBuffer.assignee = this.session.currentUser._id;
   }
 
   $onInit() {
+    this.users.list();
     if (this.$stateParams._id) {
       this.stories.read(this.$stateParams._id)
-        .then(this.storyBuffer = this.session.currentStory);
+        .then(() => this.storyBuffer = this.session.currentStory);
     }
   }
 
-  saveStory() {
-    console.log(!!this.session.currentStory._id)
+  save() {
     if (this.session.currentStory._id) {
       this.stories.update(this.storyBuffer);
     } else {
       this.stories.create(this.storyBuffer);
     }
+    this.editMode = false;
+  }
+
+  edit() {
+    this.editMode = true;
   }
 }
